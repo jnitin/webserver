b5b5f6dbf21521668d8e8b3c3ca05a383b162ff6
nitin jain <nitin.e@gmail.com>
2020-05-20 15:56:51 +0530

    Test file added

--- server.py
+++ server.py
@@ -183,7 +183,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     # Send actual content.
     def send_content(self, content, status=200):
         self.send_response(status)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/pdf")
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         self.wfile.write(content)
