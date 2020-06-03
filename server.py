cfb9bccd61ca78a9c914b26e854e63f683b450d1
nitin <nitin.e@gmail.com>
2020-06-03 19:39:38 +0530

    remove commented code

--- server.py
+++ server.py
@@ -250,15 +250,6 @@ class RequestHandler(CGIHTTPRequestHandler):
         logger.info("\n%s\nPath: %s\nHeaders:\n%s", str(self.requestline), str(self.path), str(self.headers))
         """Save a file following a HTTP PUT request"""
         filename = os.path.basename(self.path)
-
-        # Don't overwrite files
-        # if os.path.exists(filename):
-        # self.send_response(409, 'Conflict')
-        # self.end_headers()
-        # reply_body = '"%s" already exists\n' % filename
-        # self.wfile.write(reply_body.encode('utf-8'))
-        # return
-
         file_length = int(self.headers['Content-Length'])
         with open(filename, 'wb') as output_file:
             output_file.write(self.rfile.read(file_length))
